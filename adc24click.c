b515d6a34d1cc747603c218c0ec2d82497d9e866
KapilADK <153365734+KapilADK@users.noreply.github.com>
2024-09-26 20:21:58 +0200

    Update adc24click.c

--- adc24click.c
+++ adc24click.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <linux/spi/spidev.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
@@ -9,9 +8,10 @@
 
 void config_adc24_ctrl_reg(int spi_fd, uint16_t regCfg) {
 
-    regCfg &= ADC24_CTRL_REG_RESOLUTION;
+    // extract the last 12 bits from the configuration
+    regCfg &= 0x0FFF;
 
-    // data to be sent
+    // data to be sent (the control register is loaded on the first 12 clocks, therefore the data are MSB aligned)
     uint16_t data_to_send = (regCfg << 4);
 
     struct spi_ioc_transfer spi_transfer = {
@@ -24,7 +24,7 @@ void config_adc24_ctrl_reg(int spi_fd, uint16_t regCfg) {
     //send SPI message
     if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &spi_transfer) < 0) {
         
-        printf("**Failed to configure control register on ADC24 Click**\n");
+        printf("**Failed to configure control register on ADC24 Click**\n.");
     }
 }
 
@@ -37,24 +37,32 @@ void init_adc24(int spi_fd) {
 
     uint16_t dummy_data = DUMMY_HIGH;
 
-    struct spi_ioc_transfer spi_transfer = {
-        .tx_buf = (unsigned long)&dummy_data,
-        .rx_buf = 0,
+    struct spi_ioc_transfer dummy_transfer[2] = {
+    {
+        .tx_buf = (unsigned long)&dummy_data,  // First transfer
+        .rx_buf = (unsigned long)0,
         .len = 2,
         .bits_per_word = 16,
-    };
+    },
+    {
+        .tx_buf = (unsigned long)&dummy_data,  // Second transfer (same settings as first)
+        .rx_buf = (unsigned long)0,
+        .len = 2,
+        .bits_per_word = 16,
+    }
+};
 
-    if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &spi_transfer) < 0) {
+    if (ioctl(spi_fd, SPI_IOC_MESSAGE(2), &dummy_transfer) < 0) {
             printf("**Sending dummy data while initialising failed**\n");
         }
 }
 
-void read_adc24_data(int spi_fd, uint16_t *data) {
+int16_t read_adc24_data(int spi_fd) {
 
     uint16_t dummy_data = DUMMY_LOW;
 
     //buffer for receiving data
-    uint16_t rx_buf;
+    int16_t rx_buf;
 
     struct spi_ioc_transfer spi_transfer = {
         .tx_buf = (unsigned long)&dummy_data,
@@ -63,84 +71,79 @@ void read_adc24_data(int spi_fd, uint16_t *data) {
         .bits_per_word = 16,
     };
 
-    if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), spi_transfer) < 0) {
+    if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &spi_transfer) < 0) {
         printf("**Reading sampled data failed***\n");
         return;
     }
 
-    // extract only the last 12 bits, since the first 4 bits are channel identifier
-    *data = rx_buf & ADC24_ADC_RESOLUTION;
+    // return only the binary representation of voltage (remove the first 4 channel identifier)
+    return (rx_buf & 0x0FFF);
     
 }
 
-int convert_data_to_mV(uint16_t data) {
+int convert_raw16_to_mV(int16_t raw_data) {
 
-    int voltage =  (int) data / ADC24_HIGHEST_VAL_TWOS_COMP * ADC24_VREF_2V5 * 1000;
-    return voltage;
-
+    int voltage_mV = 0;
+    voltage_mV = (int) (((double)raw_data / ADC24_MAX_VALUE) * ADC24_VOLTAGE_RANGE * 1000);
+    return voltage_mV;
 }
 
 int get_voltage_adc24(int spi_fd, int ch) {
 
-    if (ch < 0 || ch > 15) {
-        printf("**Invalid channel number: %d**\n", ch);
+    int voltage_mV;
+
+    // check if valid channel is given
+    if (ch < 0 || ch > 15 ){
+        printf("**Invalid channel: %d**\n", ch);
         return -1;
     }
 
     // prepare configuration for control register with the given channel
-    ch = ch << 6;
-    uint16_t regCfg = ADC24_MANUAL_CONFG | ch;
+    uint16_t regCfg = ADC24_DEF_MANUAL_MODE | (uint16_t)(ch << 6);
 
     //write the configuration in the control register
     config_adc24_ctrl_reg(spi_fd, regCfg);
 
     //read back voltage
-    uint16_t data_read = DUMMY_LOW;
-    read_adc24_data(spi_fd, &data_read);
+    int16_t raw_data;
+    raw_data = read_adc24_data(spi_fd);
 
-    // check if the 12th bit is set 
-    if ((data_read & (1 << 11))!= 0) {
-        data_read = data_read - (1 << 12);
+    // check if the number is negative
+    if ((raw_data & (1 << 11)) != 0) {
+        raw_data |= 0xF000; 
     }
-
-    int voltage = convert_data_to_mV(data_read);
-    return voltage;
-
+    
+    voltage_mV = convert_raw16_to_mV(raw_data);
+    return voltage_mV;
 }
 
-void sample_sequence_adc24(int spi_fd,  int stop_ch, int channels, int *voltages) {
-    
-    //check if channels are valid
-    if (channels <=0 || channels > MAX_CHANNELS) {
-        printf("**Invalid number of channels: %d**\n", channels);
+void sequence_mode_adc24(int spi_fd, int stop_ch, int *voltages_mV, bool verbose) {
+
+    // check if the stop channel is valid
+    if (stop_ch < 1 || stop_ch > 15) {
+        printf("**Invalid stop channel: %d**\n");
         return;
     }
-    
-    // prepare configuration for control register with the sequence mode selected and the given stop channel
-    stop_ch = stop_ch << 6;
-    uint16_t regCfg = ADC24_SEQUENCE_CONFG | stop_ch;
 
-    // write the configuration in the control register
+    //prepare control register configuration with sequence mode selected 
+    uint16_t regCfg = ADC24_DEF_SEQUENCE_MODE | (uint16_t)(stop_ch << 6);
+
+    //write the config in the control register
     config_adc24_ctrl_reg(spi_fd, regCfg);
 
-    // read the voltages in a loop 
-    uint16_t data_read = DUMMY_LOW;
-    for (int i = 0; i < channels; i++) {
-        read_adc24_data(spi_fd, &data_read);
+    // read the voltages in a loop
+    int16_t raw_data;
+    for (int i = 0; i <= stop_ch; i++) {
+        raw_data = read_adc24_data(spi_fd);
 
-        //check if the 12th bit is set
-        if ((data_read & (1 << 11)) != 0) {
-            data_read = data_read - (1 << 12);
+        // check if the number is negative
+        if ((raw_data & (1 << 11)) != 0) {
+            raw_data |= 0xF000;
         }
-        /*
-        Some delay need to be added ????
-        */
 
-
-        // convert raw ADC data to voltage and store it in the provided array
-        voltages[i] = convert_data_to_mV(data_read);
-        printf("\t Voltage measured at Channel[%d] of ADC24: %dmV\n", i, voltages[i]);
-
+        voltages_mV[i] = convert_raw16_to_mV(raw_data);
+        if (verbose) {
+            printf("Voltage measured at channel %d: %dmV\n", i, voltages_mV[i]);
+        }
     }
-
 }
